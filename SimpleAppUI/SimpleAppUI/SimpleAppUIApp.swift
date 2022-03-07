3e109221a5c0f261705d5df1138bc511460d298a
Roger Olivares <olivares.roger90@gmail.com>
2022-03-06 20:24:27 -0800

    First Commit - Test

---
+++ SimpleAppUI/SimpleAppUI/SimpleAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppUIApp.swift
+//  SimpleAppUI
+//
+//  Created by Roger Olivares on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
